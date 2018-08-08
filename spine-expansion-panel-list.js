99b137580f7c491783b453eaf1dc102bb03347b4
Dmitry Pikhulya <dmitry.pikhulya@teamdev.com>
2018-08-08 17:33:30 +0300

    Fix the "Setting properties ... considered harmful" console warning
    This warning is displayed by LitElement when property change handler
    (an implementation of `_propertiesChanged` method) triggers another
    property change (for a "managed" property registered in the `properties`
    hash).
    
    To solve this, the intermediate `_expandedItem` property, which was
    changed in response to `expandedItem` property change, was removed.
    The `_handleExpandedItemChange` method, which relied on setting this
    property to trigger the element UI update in the middle of method's '
    lifecycle was reworked to trigger the UI update by invoking the
    (injected) `super._propertiesChanged` method in the respective place.

--- spine-expansion-panel-list.js
+++ spine-expansion-panel-list.js
@@ -7,7 +7,6 @@
 import {render} from 'lit-html/lit-html.js';
 import {LitElement, html} from '@polymer/lit-element';
 import '@polymer/paper-styles/shadow.js';
-import { microTask } from '@polymer/polymer/lib/utils/async.js';
 
 /**
  * An element that displays an associated array of items as a list of panels showing a summary view
@@ -38,6 +37,10 @@ import { microTask } from '@polymer/polymer/lib/utils/async.js';
  * </spine-expansion-panel-list>
  * ```
  *
+ * This element dispatches the non-bubbling `expanded-item-changed` event when the expanded item is
+ * changed. You can read the `event.detail.expandedItem` property from the dispatched `event` to
+ * detect which item has been expanded (will be `null` if no items are expanded).
+ *
  * ### Styling
  *
  * The templates provided are stamped in the light DOM of the `spine-expansion-panel-list` element,
@@ -61,20 +64,13 @@ class SpineFloatingExpansionList extends LitElement {
       items: Array,
       expandedItem: Object,
       renderCollapsedItem: Function,
-      renderExpandedItem: Function,
-      /**
-       * This property is an internal mirror of the `expandedItem` property, and it is needed
-       * instead of just using the `expandedItem` property in order to be able to measure and
-       * update item heights before the `_expandedItem` that is bound to the elements is
-       * changed, which is needed for a proper item height animation (see the
-       * `_handleExpandedItemChange` method).
-       */
-      _expandedItem: Object
+      renderExpandedItem: Function
     }
   }
 
   constructor() {
     super();
+    this.expandedItem = null;
     this.items = [];
     this._handleDocumentClick = this._handleDocumentClick.bind(this);
   }
@@ -88,24 +84,24 @@ class SpineFloatingExpansionList extends LitElement {
           ---spine-epl-divider-color: rgba(0, 0, 0, var(--dark-divider-opacity, 0.12));
         }
   
-        #container ::slotted(.-spine-expansion-panel-list-item) {
+        #container ::slotted(.-spine-expansion-panel--list-item) {
           margin: 0 var(--spine-expansion-panel-list-expansion-size, 20px);
           @apply --shadow-elevation-2dp;
           background: var(--primary-background-color, #ffffff);
           color: var(--primary-text-color, #000000);
           padding: 8px 16px;
           cursor: pointer;
-          transition: all 0.2s;
+          transition: all 5.2s;
   
           @apply --spine-expansion-panel-list-item;
           overflow: hidden;
         }
   
-        #container ::slotted(.-spine-expansion-panel-list-item:not([expanded]):not([ends-collapsed-range])) {
+        #container ::slotted(.-spine-expansion-panel--list-item:not([expanded]):not([ends-collapsed-range])) {
           border-bottom: 1px solid var(---spine-epl-divider-color);
         }
   
-        #container ::slotted(.-spine-expansion-panel-list-item[expanded]) {
+        #container ::slotted(.-spine-expansion-panel--list-item[expanded]) {
           margin: 16px 0;
           @apply --shadow-elevation-8dp;
   
@@ -131,20 +127,20 @@ class SpineFloatingExpansionList extends LitElement {
    * This is needed for their style to be customizable with CSS declarations present in the
    * context where the `spine-expansion-panel-list` element is used.
    */
-  _renderLightDOM({items, _expandedItem, renderCollapsedItem, renderExpandedItem}) {
+  _renderLightDOM({items, expandedItem, renderCollapsedItem, renderExpandedItem}) {
     return html`${
-      items.map(item => html`
-        <div class="-spine-expansion-panel-list-item" 
-             expanded?="${(item === _expandedItem)}" 
-             ends-collapsed-range?="${this._getItemEndsCollapsedRange(item, _expandedItem)}" 
+        items.map(item => html`
+        <div class="-spine-expansion-panel--list-item" 
+             expanded?="${(item === expandedItem)}" 
+             ends-collapsed-range?="${this._getItemEndsCollapsedRange(item)}" 
              on-click="${e => this._handleItemClick(item)}">
-          <div class="-spine-expansion-panel-list-item-content">
+          <div class="-spine-expansion-panel--list-item-content">
             <!--
               The "overflow: hidden" style is added below to prevent collapsing the stamper
               children's margins, e.g. if <h2> is placed as the first template's tag,
               see this approach here: https://stackoverflow.com/a/19719427
               This is needed for a proper "height: auto" animation in
-              \`_handleExpandedItemChange\` method.
+              the \`_handleExpandedItemChange\` method.
 
               Inplace style is used instead of a dedicated CSS rule since this template is
               rendered in the element's light DOM (not shadow DOM), and the ::slotted CSS
@@ -152,14 +148,14 @@ class SpineFloatingExpansionList extends LitElement {
               https://developers.google.com/web/fundamentals/web-components/shadowdom#stylinglightdom
             -->
             <div style="overflow: hidden">${
-              item !== _expandedItem || !this.renderExpandedItem
+            item !== expandedItem || !this.renderExpandedItem
                 ? renderCollapsedItem(item)
                 : renderExpandedItem(item)
             }</div>
           </div>
         </div>
       `)
-    }`;
+        }`;
   }
 
   /**
@@ -170,9 +166,9 @@ class SpineFloatingExpansionList extends LitElement {
     super._applyRender(result, node);
 
     // render light DOM tree
-    const {items, _expandedItem, renderCollapsedItem, renderExpandedItem} = this;
+    const {items, expandedItem, renderCollapsedItem, renderExpandedItem} = this;
     const lightDOMTemplateResult = this._renderLightDOM(
-        {items, _expandedItem, renderCollapsedItem, renderExpandedItem}
+        {items, expandedItem, renderCollapsedItem, renderExpandedItem}
     );
     render(lightDOMTemplateResult, this);
   }
@@ -188,12 +184,24 @@ class SpineFloatingExpansionList extends LitElement {
   }
 
   _propertiesChanged(props, changedProps, oldProps) {
-    super._propertiesChanged(props, changedProps, oldProps);
+    const invokeSuperPropertiesChanged = () => {
+      super._propertiesChanged(props, changedProps, oldProps);
+    };
     if (changedProps && changedProps.expandedItem !== undefined) {
-      this._handleExpandedItemChange();
+      this._handleExpandedItemChange(oldProps.expandedItem, invokeSuperPropertiesChanged);
+    } else {
+      invokeSuperPropertiesChanged();
     }
   }
 
+  /**
+   * @param {Object} item          an item to be tested
+   * @param {Object} expandedItem  the current expanded item
+   * @return {boolean} `true` if item specified by the `item` parameter is the last one in a series
+   *                          of consecutive collapsed items (e.g. a last item or an item before an
+   *                          expanded one)
+   * @private
+   */
   _getItemEndsCollapsedRange(item, expandedItem) {
     const itemIndex = this.items.indexOf(item);
     if (itemIndex === -1) {
@@ -211,20 +219,92 @@ class SpineFloatingExpansionList extends LitElement {
     return itemIndex === expandedItemIndex - 1;
   }
 
-  _handleItemClick(item) {
-    this.expandedItem = item;
-    this.dispatchEvent(new CustomEvent('expanded-item-changed'));
+  /**
+   * This function is invoked when the `expandedItem` property is changed. It ensures that item
+   * heights are animated properly for both old and new expanded items without quick "jumps" in
+   * heights.
+   *
+   * A component is in the following state when this method is invoked:
+   * - the `expandedItem` property contains a new value;
+   * - a component's UI hasn't been rendered in response to a property change yet: an implementation
+   *   of this method is responsible of invoking the `renderUpdatedUI` function passed to it to
+   *   render the updated UI accordingly.
+   *
+   * @param {Object}   prevExpandedItem  A previous value of the `expandedItem` property.
+   * @param {Function} renderUpdatedUI   A function that synchronously applies rendering according
+   *                                     to the pending property updates (that include an updated
+   *                                     `expandedItem` property).
+   * @private
+   */
+  _handleExpandedItemChange(prevExpandedItem, renderUpdatedUI) {
+    const setItemHeightByContentHeight = itemElement => {
+      const itemContentElement = this._getItemContentElement(itemElement);
+      itemElement.style.height = `${itemContentElement.offsetHeight}px`;
+    };
+    const setItemHeightAuto = itemElement => {
+      itemElement.style.height = 'auto';
+    };
+
+    const prevExpandedElement = this._getItemElement(prevExpandedItem);
+    const newExpandedElement = this._getItemElement(this.expandedItem);
+    const itemHeightsToAnimate = [prevExpandedElement, newExpandedElement].filter(el => el);
+
+    // we need to set the fixed initial item heights instead of 'auto' values in order for
+    // the height transition animation to work (which doesn't work with 'auto' values)
+    itemHeightsToAnimate.forEach(setItemHeightByContentHeight);
+
+    renderUpdatedUI();
+
+    // start the height transition animation by setting height to match the updated (expanded or
+    // collapsed) content height
+    itemHeightsToAnimate.forEach(setItemHeightByContentHeight);
+
+    // when animation completes, reset item heights from fixed values back to 'auto' for any
+    // subsequent height changes that might occur dynamically are not ignored (e.g. if item content
+    // changes dynamically after this)
+    const transitionDuration =
+        this.constructor.__getElementTransitionDuration(itemHeightsToAnimate[0]);
+    setTimeout(() => {
+      itemHeightsToAnimate.forEach(setItemHeightAuto);
+    }, transitionDuration);
   }
 
+  /**
+   * @return {NodeList} a [NodeList](https://developer.mozilla.org/en-US/docs/Web/API/NodeList)
+   *                    containing all of the item elements currently displayed by this component
+   */
   _getItemElements() {
-    return this.querySelectorAll('.-spine-expansion-panel-list-item');
+    return this.querySelectorAll('.-spine-expansion-panel--list-item');
   }
 
+  /**
+   * @param {Object} item  an item object, as found in the `items` array, whose element should be
+   *                       located
+   * @return {Element|undefined} an element that displays the provided item, or `undefined` if the
+   *                             passed item is not in the current list of items
+   */
+  _getItemElement(item) {
+    const itemElements = this._getItemElements();
+    return itemElements[this.items.indexOf(item)];
+  }
+
+  /**
+   * @param {Element} itemElement
+   * @return {Element}
+   */
   _getItemContentElement(itemElement) {
-    return itemElement.querySelector('.-spine-expansion-panel-list-item-content');
+    return itemElement.querySelector('.-spine-expansion-panel--list-item-content');
   }
 
-  static __getTransitionDuration(element) {
+  /**
+   * Takes the current value of element's `transition-duration` property and returns a respective
+   * duration value in milliseconds if present, or 0 otherwise.
+   *
+   * @param {Element} element an element to be analyzed
+   * @return {number} a transition duration value in milliseconds
+   * @private
+   */
+  static __getElementTransitionDuration(element) {
     if (!element) {
       return 0;
     }
@@ -241,44 +321,17 @@ class SpineFloatingExpansionList extends LitElement {
     }
   }
 
-  _handleExpandedItemChange() {
-    const itemElements = this._getItemElements();
-    const elementForItem = (item, indexCorrection = 0) =>
-        itemElements[this.items.indexOf(item) + indexCorrection];
-    const prevExpandedElement = elementForItem(this._expandedItem);
-    const newExpandedElement = elementForItem(this.expandedItem);
-
-    const itemHeightsToAnimate = [prevExpandedElement, newExpandedElement].filter(el => el);
-
-    const setItemHeightByContent = element => {
-      const itemContentElement = this._getItemContentElement(element);
-      element.style.height = `${itemContentElement.offsetHeight}px`;
-    };
-    const setItemHeightAuto = element => {
-      element.style.height = 'auto';
-    };
-
-    // we need to set the fixed initial item heights instead of 'auto' values in order for
-    // the height transition animation to work (which doesn't work with 'auto' values)
-    itemHeightsToAnimate.forEach(setItemHeightByContent);
-    this._expandedItem = this.expandedItem;
-    microTask.run(() => {
-      if (!this.ownerDocument) {
-        // skip further animation if the element was removed from the document already
-        return;
+  _setExpandedItem(item) {
+    this.expandedItem = item;
+    this.dispatchEvent(new CustomEvent('expanded-item-changed', {
+      detail: {
+        expandedItem: item
       }
-      // start the height transition animation by setting height to match the updated content;
-      // this should be done after an async delay to let the `dom-if` elements to restamp the
-      // contents of expanded/collapsed items before we measure their new heights
-      itemHeightsToAnimate.forEach(setItemHeightByContent);
-      const transitionDuration =
-          this.constructor.__getTransitionDuration(itemHeightsToAnimate[0]);
-      setTimeout(() => {
-        // remove fixed item heights and set them back to 'auto' for any subsequent height
-        // changes that might occur dynamically (e.g. due to content changes) are not ignored
-        itemHeightsToAnimate.forEach(setItemHeightAuto);
-      }, transitionDuration);
-    });
+    }));
+  }
+
+  _handleItemClick(item) {
+    this._setExpandedItem(item);
   }
 
   _handleDocumentClick(event) {
@@ -288,7 +341,7 @@ class SpineFloatingExpansionList extends LitElement {
       // one of the items was clicked, no auto collapsing is required
       return;
     }
-    this.expandedItem = null;
+    this._setExpandedItem(null);
   }
 }
 
